8a2db2a684b868d068526bbbae3dcaa02daf4f94
Ikram Kharbouch <ikrkharb@e1r9p8.1337.ma>
2019-05-02 15:45:26 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ikrkharb <ikrkharb@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 22:20:04 by ikrkharb          #+#    #+#             */
-/*   Updated: 2019/04/26 14:27:59 by ikrkharb         ###   ########.fr       */
+/*   Updated: 2019/04/27 16:59:22 by ikrkharb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int		ft_is_found(char **line, char *buff, char *pos)
 	char	*sub_str;
 
 	if (!(sub_str = ft_strsub(buff, 0, pos - buff)))
-		return (0);
+		return (-1);
 	if (!(new = ft_strjoin(*line, sub_str)))
 	{
 		ft_strdel(&sub_str);
-		return (0);
+		return (-1);
 	}
 	ft_strdel(line);
 	ft_strdel(&sub_str);
@@ -31,15 +31,15 @@ int		ft_is_found(char **line, char *buff, char *pos)
 	return (1);
 }
 
-int		ft_not_found(char **line, char *buff, int size)
+int		ft_not_found(char **line, char *buff)
 {
 	char	*tmp;
 
 	if (!(tmp = ft_strjoin(*line, buff)))
-		return (0);
+		return (-1);
 	ft_strdel(line);
 	*line = tmp;
-	ft_bzero(buff, size);
+	ft_bzero(buff, BUFF_SIZE + 1);
 	return (1);
 }
 
@@ -49,23 +49,25 @@ int		get_next_line(const int fd, char **line)
 	int			size;
 	char		*pos;
 
-	if (fd < 0 || read(fd, NULL, 0) == -1)
+	if (fd < 0 || read(fd, NULL, 0) == -1 || !(*line = ft_strnew(0)))
 		return (-1);
-	if (!line)
-		return (-1);
-	*line = buff[0] ? ft_strdup(buff) : ft_strdup("");
+	if (*buff)
+	{
+		if ((pos = ft_strchr(buff, '\n')))
+			return (ft_is_found(line, buff, pos));
+		else if (ft_not_found(line, buff) == -1)
+			return (-1);
+	}
 	while ((size = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[size] = '\0';
 		if ((pos = ft_strchr(buff, '\n')))
-		{
-			if (!ft_is_found(line, buff, pos))
-				return (-1);
-			return (1);
-		}
-		else if (!ft_not_found(line, buff, size))
+			return (ft_is_found(line, buff, pos));
+		else if (!ft_not_found(line, buff))
 			return (-1);
 	}
+	if (**line)
+		return (1);
 	ft_strdel(line);
-	return (0);
+	return (size);
 }
